237561caee0e046e80a3b8d7679872bc7093601f
rajatb555 <rajat.bopalkar@gmail.com>
2021-08-06 19:31:56 +0530

    First Commit

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 md:px-40
+        py-14 bg-gray-100 text-gray-400">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Investors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb Luxe</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is not areal site</p>
+                <p>Its a pretty awesome clone</p>
+                <p>Referals Accepted</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>RajatB555</p>
+                <p>React App</p>
+                <p>Tailwind CSS</p>
+                <p>Netlify</p>
+                <p>Next.JS</p>
+            </div>
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Centre</p>
+                <p>Chat with us</p>
+                <p>Contact us</p>
+                <p>Email</p>
+                <p>Say hi over Instagram</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
