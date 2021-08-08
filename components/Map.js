a4bd1829a5d22a61d7a1a50edac96dfae33eddf9
rajatb555 <rajat.bopalkar@gmail.com>
2021-08-08 19:29:58 +0530

    Zoom change second

--- components/Map.js
+++ components/Map.js
@@ -21,7 +21,7 @@ function Map({ searchResults }) {
         height: "100%",
         latitude: center.latitude,
         longitude: center.longitude,
-        zoom: 6,
+        zoom: 9,
     });
 
     return (
